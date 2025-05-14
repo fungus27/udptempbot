ac2f09952cdde74c5266e1c7e18153adb347d72c
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2025-05-14 15:28:09 -0500

    implement the temperature reading struct

--- main.c
+++ main.c
@@ -2,31 +2,72 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <time.h>
 
 #include <netdb.h>
 #include <sys/socket.h>
 
-#define NETWORK_POWER 0
-#define BATTERY_POWER 1
+#define STATUS_MASK (uint16_t)(1 << 16)
+#define TEMP_MASK (uint16_t)(0xffff >> 1)
 
-struct temp_reading {
+#define NETWORK_POWER (uint16_t)(0 << 16)
+#define BATTERY_POWER (uint16_t)(1 << 16)
+
+#define COMBINE_TEMP_STATUS(temp, status) (uint16_t)( ( (temp) & TEMP_MASK ) | (status) )
+
+struct reading {
     // note that this is subject to the year 2038 problem.
     // change to uint64_t when needed.
-    uint32_t timestamp;
+    int32_t timestamp;
 
-    // 1000 values (20-120 range with 0.1 precision) require at least 2 bytes
-    uint16_t temperature;
-
-    // one of NETWORK_POWER or BATTERY_POWER
-    uint8_t status;
+    // this 16-bit value stores both the temperature and status.
+    // the status and temperature are obtained by (temp_status & STATUS_MASK), and (temp_status & TEMP_MASK).
+    //
+    // the true temperature is calculated from the formula 20.0 + (temperature_status & TEMP_MASK)/10.0.
+    // note that it has a bigger range than in the specification
+    uint16_t temp_status;
 
     // id incremented with each reading
     uint8_t id;
 
     // the checksum is computed as the negation of the sum of all the bytes of the structure.
     // to validate the message, the sum of all bytes (including the checksum) must be 0
-    uint8_t checksum;
+    int8_t checksum;
 };
+
+// computes the checksum for the reading.
+void reading_compute_checksum(struct reading *packet) {
+    packet->checksum = 0;
+    for (int i = 0; i < sizeof(*packet) - 1; ++i)
+        packet->checksum += ((int8_t*)packet)[i];
+    packet->checksum = -packet->checksum;
+}
+
+// validates the correctness of the reading.
+// the reading is valid if and only if this function returns 0.
+char reading_validate(struct reading *packet) {
+    int8_t sum = 0;
+    for (int i = 0; i < sizeof(*packet); ++i)
+        sum += ((int8_t*)packet)[i];
+    return (char)sum;
+}
+
+// print the given reading in a user-friendly way.
+void reading_display(struct reading *packet) {
+    const char *status = (packet->temp_status & STATUS_MASK) == NETWORK_POWER ? "network" : "battery";
+    const char *validity = reading_validate(packet) == 0 ? "valid" : "invalid";
+
+    // calculate the whole and decimal part of the temperature
+    unsigned short whole = 20 + (packet->temp_status & TEMP_MASK) / 10;
+    unsigned short decimal = (packet->temp_status & TEMP_MASK) % 10;
+
+    printf( "ID: %hhu\n"
+            "Timestamp: %d\n"
+            "Temperature: %hu.%hu\n"
+            "Power status: %s\n"
+            "Checksum: 0x%hhx (%s)\n",
+            packet->id, packet->timestamp, whole, decimal, status, packet->checksum, validity);
+}
 
 // returns an UDP socket descriptor for the specified ip address type or -1 on error (check errno)
 int get_udp_socket(int domain) {
@@ -91,4 +132,11 @@ int main(int argc, char *argv[]) {
 
     // close socket
     shutdown(sockfd, SHUT_RDWR);
+
+    struct reading r;
+    r.timestamp = time(NULL);
+    r.temp_status = COMBINE_TEMP_STATUS(405, NETWORK_POWER);
+    r.id = 4;
+    reading_compute_checksum(&r);
+    reading_display(&r);
 }
